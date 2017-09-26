74045a5f29e9c7f45392212e3302207b82821d1e
Jeff Liekens <liekensjeff@gmail.com>
2017-09-26 20:59:54 +0200

    Equalitity demo & Enumarable.Except() demo

---
+++ DotNet/IEquatable/IEquatable/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEquatable
+{
+    public class Person: IEquatable<Person>
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public Person(int id, string lastName, string firstName)
+        {
+            Id = id;
+            LastName = lastName;
+            FirstName = firstName;
+        }
+
+        public bool Equals(Person other)
+        {
+            return other.Id == this.Id;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return Id == ((Person)obj).Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} + {LastName} + {FirstName}";
+        }
+    }
+}
---
+++ DotNet/IEquatable/IEquatable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEquatable
+{
+    class Program
+    {
+        private const int size = 100;
+        static void Main(string[] args)
+        {
+
+            var list = new List<Person>();
+            var smallerList = new List<Person>();
+
+            //fill list 
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(new Person(i, "", ""));
+
+                if( i % 2 == 0)
+                {
+                    smallerList.Add(new Person(i, "", ""));
+
+                }
+            }
+
+            // To let the Except method work on your list with a custom object
+            // You have to override equal properly or implement it by inherriting IEquatable
+            var result = list.Except(smallerList);
+
+            foreach (var person in result)
+            {
+                Console.WriteLine(person);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
